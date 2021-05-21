f04a2d99e632b1f690c1472f71fb2fdcb1a19353
lucasmetron <lucasmetron@gmail.com>
2021-05-21 15:27:58 -0300

    inserido delete

--- model/posts.js
+++ model/posts.js
@@ -2,7 +2,7 @@ module.exports = {
     posts: [
         {
             id: "dagfafsgg",
-            titulo: "Título teste",
+            title: "Título teste",
             description: "Descrição teste"
         },
     ],
@@ -13,11 +13,18 @@ module.exports = {
 
     newPost(title, description) {
         this.posts.push({ id: generateID(), title, description })
+    },
+
+    deletePost(id) {
+        this.posts.forEach((item, i) => {
+            if (item.id == id) {
+                this.posts.splice(i, 1)
+            }
+            console.log(this.posts);
+        })
+
     }
 }
-
-
-
 
 let generateID = () => {
     return Math.random().toString(36).substr(2, 9) //base 36 seram todas as letras do alfabeto + os 10 numeros, substr(2,9) server para desprezar as duas primeiras posições e pegar o nove restantes
