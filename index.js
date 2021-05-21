1b8235672697275d01eb80c8a668709278d07649
lucasmetron <lucasmetron@gmail.com>
2021-05-21 11:12:17 -0300

    preparando o backend para responder requisições gET

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const express = require("express")
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+let posts = [
+    {
+        id: "dagfafsgg",
+        titulo: "Título teste",
+        description: "Descrição teste"
+    },
+
+]
+
+app.get("/all", (req, res) => {
+    res.json(posts);
+});
+
+app.post("/new", (req, res) => {
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
