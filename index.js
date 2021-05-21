404aad85c76f07bf7eb9c32c88ad1bdd2d7f0e9a
lucasmetron <lucasmetron@gmail.com>
2021-05-21 15:58:26 -0300

    roteamento feito

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 const PORT = 3000;
 const express = require('express');
-// const bodyParser = require('body-parser');
-const posts = require('./model/posts');
 
 const app = express();
+const path = require('path')
+const apiRoute = require('./routers/api')
+
+app.use('/api', apiRoute)
+app.use('/', express.static(path.join(__dirname, "public")));
 
 
-
-app.get("/all", (req, res) => {
-
-    res.json(JSON.stringify(posts.getAll()));
-
-});
-
-app.post("/new", express.json(), (req, res) => { //express.json() é um middlaware que permite capturar o body da requisição
-
-    let title = req.body.title;
-    let description = req.body.description;
-
-    posts.newPost(title, description);
-
-    res.send("Post adicionado");
-
-});
-
-app.delete("/deletePost", express.json(), (req, res) => {
-    let id = req.body.id;
-
-    posts.deletePost(id);
-
-    res.send(`ID: ${id} deletado!`)
-})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
---
+++ routers/api.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const posts = require('../model/posts');
+
+router.get("/all", (req, res) => {
+
+    res.json(JSON.stringify(posts.getAll()));
+
+});
+
+router.post("/new", express.json(), (req, res) => { //express.json() é um middlaware que permite capturar o body da requisição
+
+    let title = req.body.title;
+    let description = req.body.description;
+
+    posts.newPost(title, description);
+
+    res.send("Post adicionado");
+
+});
+
+router.delete("/deletePost", express.json(), (req, res) => {
+    let id = req.body.id;
+
+    posts.deletePost(id);
+
+    res.send(`ID: ${id} deletado!`)
+})
+
+
+module.exports = router;
