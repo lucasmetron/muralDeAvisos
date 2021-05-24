dd266f48de9b17477f381798b49f0dd4586f45ca
lucasmetron <lucasmetron@gmail.com>
2021-05-24 15:16:31 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const PORT = 3000;
 const express = require('express');
-
 const app = express();
 const path = require('path')
 const apiRoute = require('./routers/api')
 
-app.use('/api', apiRoute)
+app.use('/api', apiRoute);
+
 app.use('/', express.static(path.join(__dirname, "public")));
-
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
