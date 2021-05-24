c0f6d5b25fe377d01b9aaffe4ed37b9ec810c6cf
lucasmetron <lucasmetron@gmail.com>
2021-05-24 14:47:42 -0300

    função delete, implementada

--- public/script/script.js
+++ public/script/script.js
@@ -12,15 +12,20 @@ function updatePosts() {
         posts.forEach((post) => {
             let postElment = `
             <div id=${post.id} class="card mb-4">
-            <div class="card-header">
-                <h5 class="card-title">${post.title}</h5>
-            </div>
-            <div class="card-body">
-                <div class="card-text">
-                    ${post.description}
+                <div class="card-header">
+                    <h5 class="card-title">${post.title}</h5>
                 </div>
-            </div>
-        </div>`;
+                <div class="card-body">
+                    <div class="card-text">
+                    ${post.description}<br>
+                    
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <span class="id" >ID post: ${post.id}</span>
+                </div>
+            </div>`;
 
             postElements += postElment;
         })
@@ -36,13 +41,86 @@ function newPost() {
     let title = document.querySelector("#title").value;
     let description = document.querySelector("#desc").value;
 
-    let postOb = {
-        title,
-        description
+    if (title === "" || description === "") {
+        document.body.innerHTML += `
+        <div class="topo">
+            <div class="alert alert-danger alert-dismissible fade show" id="error">
+
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                <strong>Erro:</strong> Os campos Título e descrição, não devem estar vazios!
+
+            </div>
+        </div>`;
+
+    } else {
+
+        let post = {
+            title,
+            description
+        }
+
+        let options = {
+            method: "POST",
+            headers: new Headers({
+                "content-type": "application/json"
+            }),
+            body: JSON.stringify(post)
+        }
+
+        fetch("http://192.168.0.105:3000/api/new", options).then(res => {
+            console.log(res)
+            updatePosts();
+            document.querySelector("#title").value = "";
+            document.querySelector("#desc").value = "";
+            document.querySelector("#del").value = "";
+        })
+
+
+
     }
 
-    fetch("http://192.168.0.105:3000/api/new").then(res => {
-        console.log("deu certo")
-    })
-    console.log(postOb)
+
+
+}
+
+function del() {
+    let id = document.querySelector("#del").value;
+
+    if (id === "") {
+        document.body.innerHTML += `
+        <div class="topo">
+            <div class="alert alert-danger alert-dismissible fade show" id="error">
+
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                <strong>Erro:</strong> O campo ID, não deve estar vazio!
+
+            </div>
+        </div>`;
+    } else {
+
+        let idDel = {
+            id
+        }
+
+        let options = {
+            method: "DELETE",
+            headers: new Headers({
+                "content-type": "application/json"
+            }),
+            body: JSON.stringify(idDel)
+        }
+
+        fetch("http://192.168.0.105:3000/api/deletePost", options).then(res => {
+            console.log(res)
+            document.querySelector("#del").value = "";
+            document.querySelector("#title").value = "";
+            document.querySelector("#desc").value = "";
+            updatePosts();
+        })
+
+
+
+    }
+
+
 }
--- routers/api.js
+++ routers/api.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const posts = require('../model/posts');
+const cors = require('cors')
+const options = {
+    origin: "http://localhost:3000"
+}
 
+router.use(cors(options))
 router.get("/all", (req, res) => {
 
     res.json(JSON.stringify(posts.getAll()));
